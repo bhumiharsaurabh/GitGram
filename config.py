9ec28f30e76ec319f5c12140131f94d78678fd3d
BHUMIHAR SAURABH (KATIL) <100083442+bhumiharsaurabh@users.noreply.github.com>
2022-06-06 11:07:25 +0530

    GitGram

---
+++ config.py
@@ -0,0 +1,8 @@
+# Your Bot token in the quotes taken from @BotFather on Telegram
+BOT_TOKEN = "5586865347:AAGi0RMxY2NSkBywgmsViD8gSFKH2LD9eoU"
+
+# Project Name of your Project
+PROJECT_NAME = "GitGram"
+
+# If you forked the Repository, grab your repo's link and paste it below!
+GIT_REPO_URL = "https://github.com/bhumiharsaurabh/GitGram
